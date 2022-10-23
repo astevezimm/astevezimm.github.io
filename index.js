224efc456bc3ea039b8f7ac7752af959591e0b6d
Andrew Zimmerman <astevezimm@gmail.com>
2022-10-23 10:36:51 -0600

    implement image selection logic

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const IMG_COUNT = 20;
 
 let htmlContent = "";
 for (let i = 1; i <= IMG_COUNT; i++)
-    htmlContent += `<li><img src="images/i${i.toString().padStart(2, '0')}.jpg" alt="Leila" /></li>`;
+    htmlContent += `<li><img id="${i}" src="images/i${i.toString().padStart(2, '0')}.jpg" alt="Leila" /></li>`;
 
 document.getElementById("gallery").innerHTML = htmlContent;
+
+let index = IMG_COUNT;
+let active = document.getElementById(index);
+nextImg();
+
+function nextImg() {
+    index = index >= IMG_COUNT ? 1 : index + 1;
+    updateActive();
+}
+
+function prevImg() {
+    index = index <= 1 ? IMG_COUNT : index - 1;
+    updateActive();
+}
+
+function updateActive() {
+    active.classList.remove("active");
+    active = document.getElementById(index);
+    active.classList.add("active");
+}
