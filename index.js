854b75d656abed74c20e101991aad5e621bc0aaa
Andrew Zimmerman <astevezimm@gmail.com>
2022-10-22 21:01:22 -0600

    remove jquery, because github didn't seem to like it

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const IMG_COUNT = 20;
 
-const gallery = $("#gallery");
-console.log(gallery)
-
+let htmlContent = "";
 for (let i = 1; i <= IMG_COUNT; i++)
-    gallery.append(`<li><img src="images/i${i.toString().padStart(2, '0')}.jpg" alt="Leila" /></li>`);
+    htmlContent += `<li><img src="images/i${i.toString().padStart(2, '0')}.jpg" alt="Leila" /></li>`;
 
-console.log(gallery)
+document.getElementById("gallery").innerHTML = htmlContent;
